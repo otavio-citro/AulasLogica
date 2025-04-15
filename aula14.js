f6eb2daf4d4e3382287b09871dc630b0ce42eb9d
otavio citro magalhaes <otaviocitromagalhaes@gmail.com>
2025-04-15 15:47:40 -0300

    aula15

--- aula14.js
+++ aula14.js
@@ -70,3 +70,77 @@ console.log(data2);
 let data3 = FormatarData(4, 8, 25)
 console.log(data3);
 
+function coresprim() {
+    let cores = ['vermelho', 'amarelo', 'azul']
+    for (cor of cores)
+        console.log(cor);
+
+}
+coresprim()
+
+function tabuada(n1) {
+    for (let x = 1; x <= 10; x++) {
+        console.log(`${n1 * x}`);
+
+    }
+}
+tabuada(8)
+tabuada(20)
+tabuada(6)
+
+function divisao(n1, n2) {
+    return `${n1 / n2}`
+}
+let divisaoo = divisao(5, 10)
+console.log(divisaoo);
+
+
+
+function add(n1, n2) {
+    console.log(n1 + n2);
+
+}
+function sub(n1, n2) {
+    console.log(n1 - n2);
+
+}
+function mult(n1, n2) {
+    console.log(n1 * n2);
+
+}
+function divi(n1, n2) {
+    console.log(n1 / n2);
+
+}
+function elev(n1, n2) {
+    console.log(n1 ** n2);
+
+}
+
+function calcular(nr1, nr2, op) {
+    if (op == "+") {
+        add(nr1, nr2);
+    } else if (op == "-") {
+        sub(nr1, nr2)
+    } else if (op == "*") {
+        mult(nr1, nr2)
+    } else if (op == '/') {
+        divi(nr1, nr2)
+    } else if (op == '**') {
+        elev(nr1, nr2)
+    }
+}
+calcular(50, 50, "+")
+
+
+function senhaAleatoria(qtdCaracteres) {
+    let listaCaracteres = ['!', "a", 5, 'R', "$", '#']
+    let senha = '';
+    for (let x = 1; x <= qtdCaracteres; x++){
+        let posSorteada = Math.floor(Math.random() * 6)
+    senha += listaCaracteres[posSorteada];
+        
+    }
+return senha
+}
+console.log(senhaAleatoria(50));
